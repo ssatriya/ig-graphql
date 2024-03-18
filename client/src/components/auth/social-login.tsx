570912c30976ec10586776a00c01ba5f04fee3b6
Donny Satriya <ssatriya@gmail.com>
2024-03-19 01:50:45 +0800

    test prod env

--- client/src/components/auth/social-login.tsx
+++ client/src/components/auth/social-login.tsx
@@ -12,25 +12,28 @@ const SocialLogin = () => {
   };
 
   return (
-    <div className="flex gap-2">
-      <Button
-        onClick={() => onClick("google")}
-        className="w-full text-xs bg-primary"
-        size="sm"
-        aria-label="Login with Google"
-      >
-        Log in with Google
-      </Button>
-      <Button
-        disabled
-        onClick={() => onClick("github")}
-        className="w-full text-xs bg-primary"
-        size="sm"
-        aria-label="Login with Github"
-      >
-        Log in with Github
-      </Button>
-    </div>
+    <>
+      <p>{`${import.meta.env.VITE_BACKEND_SERVER}/api/login/google`}</p>
+      <div className="flex gap-2">
+        <Button
+          onClick={() => onClick("google")}
+          className="w-full text-xs bg-primary"
+          size="sm"
+          aria-label="Login with Google"
+        >
+          Log in with Google
+        </Button>
+        <Button
+          disabled
+          onClick={() => onClick("github")}
+          className="w-full text-xs bg-primary"
+          size="sm"
+          aria-label="Login with Github"
+        >
+          Log in with Github
+        </Button>
+      </div>
+    </>
   );
 };
 
